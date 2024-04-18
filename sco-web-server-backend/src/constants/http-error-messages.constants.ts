e0a3b69ffbdfdce64ee2bcb5c78f7577612c687b
scomeras94 <scomeras@gmail.com>
2024-04-18 18:06:53 +0200

    Balance error constants

--- sco-web-server-backend/src/constants/http-error-messages.constants.ts
+++ sco-web-server-backend/src/constants/http-error-messages.constants.ts
@@ -79,6 +79,7 @@ export const BACKEND_HTTP_ERROR_CONSTANTS = {
     ROOT_PATH_NO_FILES: 'Root path not files',
     UNNABLE_CREATE_ROOT_PATH_BACKUP: 'Unnable to create root path backup',
     UNNABLE_DOWNLOAD_FOLDER: 'Unnable to download folder',
+    UNNABLE_DOWNLOAD_FILE: 'Unnable to download file',
 
     UNNABLE_CREATE_SERVER_FOLDER: 'Unnable to create server folder',
     UNNABLE_DELETE_SERVER_FOLDER: 'Unnable to delete server folder',
--- sco-web-server-frontend/src/app/modules/shared/http-error/http-error-messages.constants.ts
+++ sco-web-server-frontend/src/app/modules/shared/http-error/http-error-messages.constants.ts
@@ -71,6 +71,7 @@ export const httpErrorMessages = {
     ROOT_PATH_NO_FILES: 'Root path not files',
     UNNABLE_CREATE_ROOT_PATH_BACKUP: 'Unnable to create root path backup',
     UNNABLE_DOWNLOAD_FOLDER: 'Unnable to download folder',
+    UNNABLE_DOWNLOAD_FILE: 'Unnable to download file',
 
     UNNABLE_CREATE_SERVER_FOLDER: 'Unnable to create server folder',
     UNNABLE_DELETE_SERVER_FOLDER: 'Unnable to delete server folder',
