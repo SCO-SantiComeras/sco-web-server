37b35b21933592ad737ca92ba9cc078e00378a33
scomeras94 <scomeras@gmail.com>
2024-04-18 16:41:52 +0200

    ADD - Download backup button & function

--- sco-web-server-frontend/src/app/modules/node-server/components/node-server/node-server.component.ts
+++ sco-web-server-frontend/src/app/modules/node-server/components/node-server/node-server.component.ts
@@ -1,3 +1,4 @@
+import { UtilsService } from 'src/app/modules/shared/utils/utils.service';
 import { cloneDeep } from 'lodash-es';
 import { AppState } from './../../../../store/app.state';
 import { Component, HostListener, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
@@ -6,10 +7,11 @@ import { Observable, Subscription } from 'rxjs';
 import { ScoConstantsService, ScoDisplayResize, ScoModalService, ScoSpinnerService, ScoToastService, ScoTranslateService } from 'sco-angular-components';
 import { NodeServerState } from '../../store/node-server.state';
 import { NodeServerFile } from '../../model/node-server-file';
-import { Copy, CreateFolder, Delete, IsDirectory, List, Move, SubscribeNodeServerWS, UnSubscribeNodeServerWS, UploadFiles } from '../../store/node-server.actions';
+import { Copy, CreateFolder, Delete, DownloadBackup, IsDirectory, List, Move, SubscribeNodeServerWS, UnSubscribeNodeServerWS, UploadFiles } from '../../store/node-server.actions';
 import { NodeServerFileFilter } from '../../model/node-server-file-filter';
 import environment from 'src/environments/environment';
 import { FILE_TYPES_CONSTANTS } from 'src/app/constants/file-types.constants';
+import { NodeServerDownload } from '../../model/node-server-download';
 
 @Component({
   selector: 'app-node-server',
@@ -66,6 +68,7 @@ export class NodeServerComponent implements OnInit, OnDestroy {
     private readonly toastService: ScoToastService,
     private readonly modalService: ScoModalService,
     private readonly translateService: ScoTranslateService,
+    private readonly utilsService: UtilsService,
     public readonly constantsService: ScoConstantsService,
   ) {
     this.subManager = new Subscription();
@@ -505,6 +508,43 @@ export class NodeServerComponent implements OnInit, OnDestroy {
     if (!$event) return;
     this.selectedActionPanelFile = undefined;
     this.modalService.close('actions-panel-file-modal');
+  }
+
+  /* Download Server Backup Actions */
+  onDownloadServerBackup() {
+    this.store.dispatch(new DownloadBackup()).subscribe({
+      next: () => {
+        const success: boolean = this.store.selectSnapshot(NodeServerState.success);
+        if (!success) {
+          this.toastService.addErrorMessage(
+            this.translateService.getTranslate('label.error'),
+            this.store.selectSnapshot(NodeServerState.errorMsg),
+          );
+          return;
+        }
+
+        const nodeServerDownload: NodeServerDownload = this.store.selectSnapshot(NodeServerState.nodeServerDownload);
+        if (!nodeServerDownload) {
+          this.toastService.addErrorMessage(
+            this.translateService.getTranslate('label.error'),
+            this.store.selectSnapshot(NodeServerState.errorMsg),
+          );
+          return;
+        }
+
+        this.utilsService.downloadFile(nodeServerDownload.base64, nodeServerDownload.fileName);
+        this.toastService.addSuccessMessage(
+          this.translateService.getTranslate('label.success'),
+          this.store.selectSnapshot(NodeServerState.successMsg),
+        );
+      },
+      error: () => {
+        this.toastService.addErrorMessage(
+          this.translateService.getTranslate('label.error'),
+          this.store.selectSnapshot(NodeServerState.errorMsg),
+        );
+      }
+    })
   }
 
   /* Keyboard Events */
